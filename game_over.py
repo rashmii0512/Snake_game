0b8bd9429bee569a1547f4ab932bc27a8fbc3623
rashmii0512 <124621178+rashmii0512@users.noreply.github.com>
2023-09-12 23:35:04 +0530

    Update game_over.py

--- game_over.py
+++ game_over.py
@@ -1,16 +1,1 @@
-from turtle import Turtle
-ALIGN = "center"
-BIG_FONT = ("Courier", 30, "normal")
-SMALL_FONT = ("Courier", 18 , "normal")
 
-class GameOver(Turtle):
-    def __init__(self):
-        super().__init__()
-        self.hideturtle()
-        self.penup()
-        self.color("white")
-
-    def for_replay(self):
-        self.write("Game Over", False, align=ALIGN, font=BIG_FONT)
-        self.goto(0,-25)
-        self.write("Press enter to replay", False, align=ALIGN, font=SMALL_FONT)
